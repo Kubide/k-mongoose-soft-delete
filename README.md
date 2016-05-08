8272ef995fa299513c52c6afe316c5647fd3fe21
gelito <angel.quesada@kubide.es>
2016-05-08 02:12:43 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# k-mongoose-soft-delete
-An advanced package for softdeleting models with mongoose (also find/
+# errorhandler-express
+
+An advanced package for softdeleting models with mongoose (also 'find', 'findOne', 'findOneAndUpdate', 'update', 'count')
+
+TODO: Pending add more checks, test and documentation
+
+## Installation
+
+The best way to install it is using **npm**
+
+```sh
+npm install k-mongoose-soft-delete --save
+```
+
+## Loading
+
+```js
+var softDelete = require('k-mongoose-soft-delete');
+
+```
+
+## Initialization and Usage
+
+Basic usage (you can see how works better with [test]():
+
+```js
+mongoose.connect('mongodb://localhost/k-mongoose-soft-delete');
+
+Resource = new mongoose.Schema({
+    title: {type: String},
+    second: {type: String, soft_delete_action: 'null'},
+    third:  {type: String, soft_delete_action: 'prefix'}
+},{timestamps:true});
+
+Resource.plugin(softDelete);
+
+//When you have a resource
+resource.softDelete(function(err){});
+
+```
+
+
+## Support
+
+This plugin is proudly supported by [Kubide](http://kubide.es/) [hi@kubide.es](mailto:hi@kubide.es)
+
---
+++ test/test-1.js
@@ -0,0 +1,108 @@
+"use stric";
+
+var mongoose = require('mongoose'),
+    softDelete = require('../.'),
+    chai = require("chai"),
+    should = chai.should(),
+//async = require('async'),
+    Resource;
+
+
+/* Setup */
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/k-mongoose-soft-delete');
+
+Resource = new mongoose.Schema({
+    title: {type: String},
+    second: {type: String, soft_delete_action: 'null'},
+    third:  {type: String, soft_delete_action: 'prefix'}
+},{timestamps:true});
+
+Resource.plugin(softDelete);
+mongoose.model('Resource', Resource);
+
+/*
+ https://www.youtube.com/watch?v=--UPSacwPDA
+ Am I wrong, fallin' in love with you,
+ tell me am I wrong, well, fallin' in love with you
+ While your other man was out there,
+ cheatin' and lyin', steppin' all over you
+
+ Uh, sweet thing
+ Tell me am I wrong, holdin' on to you so tight,
+ Tell me, tell me, am I wrong, holdin' on to you so tight
+ If your other man come to claim you,
+ he'd better be ready, ready for a long long fight
+ */
+
+/* Tests */
+var title = 'Am I wrong, fallin\' in love with you!',
+    second = 'tell me am I wrong, well, fallin\' in love with you',
+    third = 'While your other man was out there',
+    resource = {};
+
+
+describe('Default plugin usage', function () {
+    before(function (done) {
+        mongoose.model('Resource').remove({}, function () {
+            done();
+        });
+    });
+
+/*    after(function (done) {
+        mongoose.model('Resource').remove({}, function () {
+            done();
+        });
+    });
+*/
+    it('Create a new resource', function (done) {
+        mongoose.model('Resource').create({
+            title: title,
+            second : second,
+            third: third
+        }, function (err, doc) {
+            should.not.exist(err);
+            should.exist(doc);
+            doc.should.have.property('title').and.equal(title);
+            done();
+        });
+    });
+
+    it('Check the resource is in the lists', function (done) {
+        mongoose.model('Resource').find({
+            title: title
+        }).exec(function (err, doc) {
+            should.not.exist(err);
+            should.exist(doc);
+            doc.should.be.an('array')
+                .with.deep.property('[0]')
+                .that.have.property("title",title);
+
+            resource = doc[0];
+            done();
+        });
+    });
+
+    it('Softdelete the resource', function (done) {
+        resource.softDelete(function(err){
+            should.not.exist(err);
+            resource.should.have.property('deleted').and.equal(true);
+            resource.should.have.property('second').and.equal(null);
+            resource.should.have.property('third').and.not.equal(third);
+            done();
+        });
+    });
+
+
+    it('Don\'t find the resource', function (done) {
+        mongoose.model('Resource').find({
+            title: title
+        }).exec(function (err, doc) {
+            should.not.exist(err);
+            should.exist(doc);
+            doc.should.be.an('array').with.length(0);
+            done();
+        });
+    });
+
+});
